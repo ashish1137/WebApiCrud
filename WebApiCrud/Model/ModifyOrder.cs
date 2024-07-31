d341ce20bd8b0d0e6c4e6badf00f47bc744d03c5
Ashish Kumar <ashish.kumar@xero.com>
2024-07-31 16:23:36 +1000

    Initial draft

---
+++ WebApiCrud/Model/ModifyOrder.cs
@@ -0,0 +1,10 @@
+﻿namespace ecommerce.Model
+{
+    public class ModifyOrder
+    {
+        public string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Description { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ WebApiCrud/Model/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace ecommerce.Model
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Description { get; set; }
+        public int Quantity { get; set; }
+    }
+}
